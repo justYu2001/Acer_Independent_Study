3bf9a985a25ed4538c030db5e16e426f04219318
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-09-06 20:45:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World');
 })
+var port = process.env.PORT || 3000;
 
-var server = app.listen(8081, function () {
+var server = app.listen(port, function () {
 
     var host = server.address().address
-    var port = server.address().port
 
-    console.log("Server running at http://127.0.0.1:8081", host, port)
+    //console.log("Server running at http://127.0.0.1:8081", host, port)
 
 })
