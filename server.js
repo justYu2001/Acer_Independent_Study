4434f92f55e33085b7db7c8cdfb2a25bbb6230c5
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-09-06 21:37:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/index.html', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 })
 var port = process.env.PORT || 3000;
