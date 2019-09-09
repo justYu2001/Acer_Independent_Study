586da0e35bc802b449a3f7993a6bfe5b28f1f79d
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-09-09 20:17:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,51 +18,6 @@ app.get('/index.html', function (req, res) {
 app.get("/test:pos", function (req, res) {
     res.send(str[req.params.pos]);
 })
-/*
-app.get("/add_device:device_id/:Raspberry_Pi_id", function (app_req, app_res) {
-    const newdevice = new device({
-        _id: app_req.params.device_id,
-        status: false
-    });
-    newdevice.save(function (f_err, f_res) {
-
-        if (f_err) {
-            app_res.send("Error:" + f_err);
-            console.log("Error:" + f_err);
-        }
-        else {
-            app_res.send("Res:" + f_res);
-            console.log("Res:" + f_res);
-        }
-
-    });
-    var wherestr = { "_id": app_req.params.Raspberry_Pi_id };
-    var updatestr = { "$push": { "device": app_req.params.device_id } };
-    Raspberry_Pi_list.find(wherestr, function (f_err, f_res) {
-
-        if (f_err) {
-            console.log("Error:" + f_err);
-        }
-        else {
-            console.log("Res:" + f_res);
-            console.log(f_res[0].get('device').includes(app_req.params.device_id));
-            if (!f_res[0].get('device').includes(app_req.params.device_id)) {
-                Raspberry_Pi_list.findByIdAndUpdate(app_req.params.Raspberry_Pi_id, updatestr, function (add_err, add_res) {
-
-                    if (add_err) {
-                        console.log("Error:" + add_err);
-                    }
-                    else {
-                        console.log("Res:" + add_res);
-                    }
-
-                });
-            }
-        }
-    });
-
-})
-*/
 
 add_device(app);
 
