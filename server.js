3d33bd578cee3feda03c269f580dacf4aeb99528
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-09-10 10:27:34 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var mongoose = require("mongoose");
 const Raspberry_Pi_list = require('./data_modules/Raspberry_Pi_list.js');
 const device = require('./data_modules/device.js');
 const add_device = require('./events/add_device.js');
-const add_Raspberry_PI = require('./events/add_Raspberry_PI.js')
+const add_Raspberry_PI = require('./events/add_Raspberry_PI.js');
+const add_user = require('./events/add_user.js');
 
 var app = express();
 
@@ -17,22 +18,7 @@ app.get('/', function (req, res) {
 
 add_device(app);
 add_Raspberry_PI(app);
-
-app.get("/add_user:user_id/:Raspberry_Pi_id", function (app_req, app_res) {
-    var wherestr = { "_id": app_req.params.Raspberry_Pi_id };
-    var updatestr = { "$push": { "user": app_req.params.user_id } };
-    const newuser = new Raspberry_Pi_list();
-    newuser.update(wherestr, updatestr, function (f_err, f_res) {
-
-        if (f_err) {
-            app_res.send("Error:" + f_err);
-        }
-        else {
-            app_res.send("Res:" + f_res);
-        }
-
-    });
-})
+add_user(app);
 
 var port = process.env.PORT || 3000;
 
