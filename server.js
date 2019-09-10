dd047f55c703861eba942c6b2b9f97517f5dbcd7
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-09-10 13:13:11 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var mongoose = require("mongoose");
-const Raspberry_Pi_list = require('./data_modules/Raspberry_Pi_list.js');
-const device = require('./data_modules/device.js');
 const add_device = require('./events/add_device.js');
 const add_Raspberry_PI = require('./events/add_Raspberry_PI.js');
 const add_user = require('./events/add_user.js');
 const check_status = require('./events/check_status.js');
+const change_status = require('./events/change_status.js');
 
 var app = express();
 
@@ -21,6 +20,7 @@ add_device(app);
 add_Raspberry_PI(app);
 add_user(app);
 check_status(app);
+change_status(app);
 
 var port = process.env.PORT || 3000;
 
