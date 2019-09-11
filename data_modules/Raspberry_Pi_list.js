4e4f39698af2e7e68c95e6972de1c66cf07ccf98
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-09-11 14:37:12 +0800

    Add files via upload

--- data_modules/Raspberry_Pi_list.js
+++ data_modules/Raspberry_Pi_list.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Raspberry_Pi_Schema = mongoose.Schema({
     _id: String,
     user: Array,
-    device: Array
+    device: [{
+        _id: String,
+        status: Boolean
+    }]
 });
 
 module.exports = mongoose.model('Raspberry_Pi_list', Raspberry_Pi_Schema);
