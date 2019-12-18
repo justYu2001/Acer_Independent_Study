d7e7cca6075937b2360de6243df1694b6d904770
justYu2001 <49834964+justYu2001@users.noreply.github.com>
2019-12-18 13:34:28 +0800

    Add files via upload

--- events/style_sheets.js
+++ events/style_sheets.js
@@ -1,9 +1,5 @@
 module.exports=(app,root_path)=>{
-    app.get('/views/device_style.css', function (req, res) {
-        res.sendFile(root_path + "/views/device_style.css");
-    })
-    
-    app.get('/views/devices_list_style.css', function (req, res) {
-        res.sendFile(root_path + "/views/devices_list_style.css");
+    app.get('/views/:file_name', function (req, res) {
+        res.sendFile(root_path + "/views/"+req.params.file_name);
     })
 }
